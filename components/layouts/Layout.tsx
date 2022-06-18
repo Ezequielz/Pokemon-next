1971e130c5c3219898e5c5ed0cfc2de016049263
Ezequiel <zapata.ed1989@gmail.com>
2022-06-18 05:22:07 -0300

    app static

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,33 @@
+import { FC } from "react"
+import Head from "next/head"
+import { Navbar } from '../ui';
+
+interface Props {
+    children: JSX.Element;
+    title?: string;
+}
+
+
+export const Layout: FC<Props> = ({ children , title}) => {
+  return (
+    <>
+        <Head>
+            <title>{ title || 'Pokemon App' }</title>
+            <meta name="author" content="Ezequiel Zapata"/>
+            <meta name="description" content={ `Informacion sobre el pokemon ${ title }` }/>
+            <meta name="keywords" content={ `${ title },pokemon,pokedex ` } />
+        </Head>
+    
+       <Navbar />
+
+
+        <main style={{
+          padding: '0px 20px'
+        }}>
+            { children }
+        </main>
+
+    </>
+
+  )
+}
---
+++ pages/favorites/index.tsx
@@ -0,0 +1,14 @@
+import { Layout } from '../../components/layouts'
+
+const FavoritesPage = () => {
+  return (
+
+    <Layout  title='Pokemons - favoritos'>
+        <h1>Favoritos</h1>
+    </Layout>
+
+
+  )
+}
+
+export default FavoritesPage
