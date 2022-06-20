08cdd2ff02c63ac910d5a3e3c7cc7c89284fa41d
Ezequiel <zapata.ed1989@gmail.com>
2022-06-20 18:59:12 -0300

    ISG name and id

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -138,7 +138,8 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
             params: { name }
           })),
 
-      fallback: false
+      // fallback: false
+      fallback: 'blocking'
     }
   }
 
@@ -146,11 +147,23 @@ export const getStaticProps: GetStaticProps = async ({params}) => {
 
     const { name } = params as { name: string};
 
+  const pokemon =  await getPokemonInfo( name )
+
+  if ( !pokemon ) {
+    return  {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
+
 
     return {
         props: {
-           pokemon : await getPokemonInfo( name )
-        }
+           pokemon 
+        },
+        revalidate: 86400
     }
 }
 
